3e15303daa43bddb5fc8dacd8143b1ba774d4bf4
Phil Dennis-Jordan <phil@philjordan.eu>
2018-05-10 19:20:00 +0200

    Adds MIT & zLib licensing terms to code, fills in missing copyright notices.

--- osdictionary_util.hpp
+++ osdictionary_util.hpp
@@ -49,57 +49,10 @@ misrepresented as being the original software.
 
 */
 
-#include "osdictionary_util.hpp"
-#include <libkern/c++/OSDictionary.h>
+#prama once
 
-#define KLog(...) ({ IOLog(__VA_ARGS__); kprintf(__VA_ARGS); })
-
-void log_dictionary_contents(OSDictionary* table)
-{
-	KLog("MCTTrigger6USB::matchPropertyTable:\n");
-	if (table == nullptr)
-	{
-		KLog("[NULL dictionary]\n");
-	}
-	else
-	{
-		OSCollectionIterator* iter = OSCollectionIterator::withCollection(table);
-		
-		while (OSObject* obj = iter->getNextObject())
-		{
-			OSString* key = OSDynamicCast(OSString, obj);
-			if (key != nullptr)
-			{
-				OSObject* obj = table->getObject(key);
-				OSNumber* num = OSDynamicCast(OSNumber, obj);
-				OSBoolean* boolean = OSDynamicCast(OSBoolean, obj);
-				OSString* str = OSDynamicCast(OSString, obj);
-				if (num != nullptr)
-				{
-					KLog("'%s' -> %llu (0x%llx, %lld)\n", key->getCStringNoCopy(), num->unsigned64BitValue(), num->unsigned64BitValue(), num->unsigned64BitValue());
-				}
-				else if (boolean != nullptr)
-				{
-					KLog("'%s' -> %s\n", key->getCStringNoCopy(), boolean->getValue() ? "true" : "false");
-				}
-				else if (str != nullptr)
-				{
-					KLog("'%s' -> '%s'\n", key->getCStringNoCopy(), str->getCStringNoCopy());
-				}
-				else if (obj == nullptr)
-				{
-					KLog("'%s' -> NULL\n", key->getCStringNoCopy());
-				}
-				else
-				{
-					KLog("'%s' -> [%s @ %p]\n", key->getCStringNoCopy(), obj->getMetaClass()->getClassName(), obj);
-				}
-			}
-			else
-			{
-				KLog("[%s @ %p]\n", obj->getMetaClass()->getClassName(), obj);
-			}
-		}
-		OSSafeReleaseNULL(iter);
-	}
-}
+class OSDictionary;
+#ifdef __cplusplus
+extern "C"
+#endif
+void log_dictionary_contents(OSDictionary* table);
